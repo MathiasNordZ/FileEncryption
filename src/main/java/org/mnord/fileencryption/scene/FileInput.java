d52df7f2409d3d9e6b0f3f5ca05550e52e0fedd0
Mathias Nord <mathias.erik.nord@gmail.com>
2025-01-24 23:10:52 +0100

    Changed scene size

--- src/main/java/org/mnord/fileencryption/scene/FileInput.java
+++ src/main/java/org/mnord/fileencryption/scene/FileInput.java
@@ -12,7 +12,7 @@ public class FileInput extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/mnord/fileencryption/fileinput-view.fxml")));
-    Scene scene = new Scene(root, 1440, 1024);
+    Scene scene = new Scene(root, 1920, 1080);
     scene.getStylesheets().add("/org/mnord/fileencryption/style.css");
     stage.setTitle("File Input");
     stage.setScene(scene);
